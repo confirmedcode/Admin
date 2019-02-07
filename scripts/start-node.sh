dd73332dd41c0bec3a42d650ff9a7ecc7306578f
Johnny Lin <hijohnnylin@gmail.com>
2019-02-07 12:54:46 -0800

    Run only one instance of Admin, otherwise we'll accidentally snapshot twice per day.

--- scripts/start-node.sh
+++ scripts/start-node.sh
@@ -4,5 +4,5 @@ export NODE_ENV=production
 
 cd /home/node/admin
 pm2 delete all
-pm2 start index.js -i max --merge-logs --log ../logs/app.log
+pm2 start index.js --merge-logs --log ../logs/app.log
 pm2 save
