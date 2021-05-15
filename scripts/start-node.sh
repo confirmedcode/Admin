baf927580c50a2923ae61d849f136321476cb273
Johnny Lin <johnny@openlyoperated.org>
2021-05-14 21:23:45 -0700

    Change PM2 order

--- scripts/start-node.sh
+++ scripts/start-node.sh
@@ -5,8 +5,13 @@ export NODE_ENV=production
 set -x
 
 cd /home/node/admin
-pm2 update
 env PATH=$PATH:/usr/local/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u node --hp /home/node
 pm2 delete all
 pm2 start index.js --merge-logs --log ../logs/app.log
+
 pm2 save
+
+# Install latest pm2
+npm install pm2@latest -g
+
+pm2 update
--- scripts/update-node.sh
+++ scripts/update-node.sh
@@ -5,6 +5,3 @@ set -x
 npm install -g n
 n 12
 npm install -g npm@6
-
-# Install latest pm2
-npm install -g pm2
